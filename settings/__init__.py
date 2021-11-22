a34dd35bdb0cb6cf39434e71a787ded5e479ee35
zhiling <maizhiling@hogucloud.com>
2021-11-22 19:17:41 +0800

    remove secret

--- settings/__init__.py
+++ settings/__init__.py
@@ -9,7 +9,7 @@ MASTODON_API_BASE_URL = env.str(
     'MASTODON_API_BASE_URL', 'https://mastodon.dalaomai.cn/')
 
 
-TENCENT_CLOUD_ID = 'AKIDObaIjdGchV3KJ1ScV7RVIo32kAJBm69v'
-TENCENT_CLOUD_key = 'XnlSt2vXswjESP5UcT3MO8cplBKCn69u'
+TENCENT_CLOUD_ID = env.str('TENCENT_CLOUD_ID')
+TENCENT_CLOUD_key = env.str('TENCENT_CLOUD_key')
 
 COS_BUCKET = 'mastodon-backup-1252550862'
