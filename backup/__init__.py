e208ae355bbc87277e0b9a299a01b7890f43cf9e
zhiling <maizhiling@hogucloud.com>
2021-12-16 17:25:48 +0800

    add log

--- backup/__init__.py
+++ backup/__init__.py
@@ -4,7 +4,7 @@ import time
 
 import settings
 from clearer import clear_daily_data, clear_jandan_toot
-from utils import cos_clinet
+from utils import cos_clinet, logger
 from utils.zip import zip_folder
 
 
@@ -12,16 +12,19 @@ def backup(data_folder, key_root='backup'):
 
     now_time = datetime.datetime.now()
     zip_file_name = f'{now_time.strftime("%Y-%m-%d")}.zip'
-    backup_zip_file = zip_folder(
-        data_folder, zip_file_name)
-    cos_clinet.upload_file(
-        Bucket=settings.COS_BUCKET,
-        LocalFilePath=backup_zip_file,
-        Key=f'{key_root}/{zip_file_name}',
-        PartSize=100,
-        EnableMD5=True
-    )
-    os.remove(backup_zip_file)
+    try:
+        backup_zip_file = zip_folder(data_folder, zip_file_name)
+        cos_clinet.upload_file(
+            Bucket=settings.COS_BUCKET,
+            LocalFilePath=backup_zip_file,
+            Key=f'{key_root}/{zip_file_name}',
+            PartSize=100,
+            EnableMD5=True
+        )
+    except Exception:
+        logger.exception('backup failue')
+    finally:
+        os.remove(backup_zip_file)
 
 
 def backup_task():
